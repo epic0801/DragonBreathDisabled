c5c77887b418ed8c4e57f870c7d977d04d3e0154
epic0801 <eli.gray0801@gmail.com>
2026-01-25 15:24:00 -0500

    Implement DisableDragonBreath plugin to block dragon breath

--- src/main/java/me/elliot/disabledragonbreath/DisableDragonBreath.java
+++ src/main/java/me/elliot/disabledragonbreath/DisableDragonBreath.java
@@ -1,1 +1,29 @@
-src/main/java/me/elliot/disabledragonbreath/DisableDragonBreath.java
+package me.elliot.disabledragonbreath;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.AreaEffectCloud;
+import org.bukkit.entity.EnderDragon;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DisableDragonBreath extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getLogger().info("DisableDragonBreath enabled!");
+    }
+
+    @EventHandler
+    public void onEntitySpawn(EntitySpawnEvent event) {
+        if (event.getEntity() instanceof AreaEffectCloud) {
+            AreaEffectCloud cloud = (AreaEffectCloud) event.getEntity();
+
+            if (cloud.getSource() instanceof EnderDragon) {
+                event.setCancelled(true);
+            }
+        }
+    }
+}
